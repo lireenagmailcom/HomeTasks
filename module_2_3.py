a67cf7d789c408b0c2a59516e28819a2830c9135
lireenagmailcom <lireena@gmail.com>
2024-11-19 15:34:13 +0100

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+# Задача "Нули ничто, отрицание недопустимо!":
+
+# Напишите цикл while с соответствующими задаче условиями.
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while i <= len(my_list):
+   if my_list[i] < 0:
+       break
+   elif my_list[i] == 0:
+       i = i + 1
+       continue
+   else: print(my_list[i])
+   i = i + 1
