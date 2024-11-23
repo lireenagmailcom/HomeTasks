560afab48e693b656be35440d8408e70d07e766d
lireenagmailcom <lireena@gmail.com>
2024-11-23 21:35:40 +0100

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,14 +1,11 @@
-first = input('Введите первое число: ')
-second = input('Введите второе число: ')
-third = input('Введите третье число: ')
-#print(first, second, third)
-if first == second and first == third:
-    print(3)
-elif first == second or first == third or second == third:
-    print(2)
-elif first != second and first != third and second != third:
-    print(0)
-else:
-    print(first, second, third)
-#  В конспекте программа заканчивалась строкой "else:", но у меня требовали продолжения банкета: IndentationError: expected an indented block after 'else' statement on line 11
-# пришлось скопировать команду времен отладки. Объясните, пожалуйста, что значит IndentationError: ожидался отступ после оператора «else» в строке 11
+first = int(input('Введите первое число: '))    # ввод первого числа
+second = int(input('Введите второе число: '))   # ввод второго числа
+third = int(input('Введите третье число: '))    # ввод третьего числа
+
+if first == second and first == third:          # если все 3 числа равны между собой
+    print(3)                                    # выводится 3
+elif first == second or first == third or second == third: # если только любые 2 числа равны между собой
+    print(2)                                    # выводится 2
+else:                                           # если все 3 числа не равны между собой
+    print(0)                                    # выводится 0
+
